cd44d84e86a9ac9f5bc5198f0176ee2a0fc51b4f
NicacioLost <nicacio.tecnologia@gmail.com>
2021-02-09 15:02:36 -0300

    Atualização das Chamadas Async

--- ProAgil.API/Controllers/WeatherForecastController.cs
+++ ProAgil.API/Controllers/WeatherForecastController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ProAgil.API.Data;
 using ProAgil.API.Model;
 
 namespace ProAgil.API.Controllers
@@ -12,62 +15,45 @@ namespace ProAgil.API.Controllers
     [Route("API/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        public readonly DataContext _context;
+        public WeatherForecastController(DataContext context)
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
+            _context = context; 
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Evento>> Get() => new Evento[]
+        public async Task<IActionResult> Get() 
          {
-            new Evento(){
-                EventoId = 1,
-                Tema = "Angular e .Net Core",
-                Local = "Belo Horizonte",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular e .Net Core",
-                Local = "São Paulo",
-                Lote = "2º Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-            }
 
-         };
+          try
+          {
+          var results = await _context.Eventos.ToListAsync();
+          return Ok(results);
+              
+          }
+          catch (System.Exception)
+          {
+              
+              return this.StatusCode(StatusCodes.Status500InternalServerError,"Banco de Dados Falhou");
+          }   
+         }
 
         [HttpGet("{id}")]
-        public ActionResult<Evento> Get(int id)
-         {
-          return new Evento[]
+        public async Task<ActionResult> Get(int id)
+        {
+
+          try
           {
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e .Net Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e .Net Core",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
-            }.FirstOrDefault(x => x.EventoId == id);
+          var results =  await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+          return Ok(results);
+              
+          }
+          catch (System.Exception)
+          {
+              
+              return this.StatusCode(StatusCodes.Status500InternalServerError,"Banco de Dados Falhou");
+          }   
+          
          }
     }   
 
