cb6568b553eacbf9e4f4220dccffe7a13391d25b
NicacioLost <nicacio.tecnologia@gmail.com>
2021-03-09 12:26:39 -0300

    Aula 4 Completa

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,25 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any = [
-  /*
-    {
-    EventoId: 1,
-    Tema: 'Angular',
-    Local: 'Belo Horizonte' 
-  },  
-  {
-    EventoId: 2,
-    Tema: '.NET Core',
-    Local: 'São Paulo' 
-  },
-  {
-    EventoId: 3,
-    Tema: 'Angular e .NET Core',
-    Local: 'Rio de Janeiro' 
+  _filtroLista: string = "";
+  
+  get filtroLista(): string{
+      return this._filtroLista;
   }
-  */
-];
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
+  eventos: any = [];
+  imagemLargura = 60;
+  imagemMargem  = 2;
+  mostrarImagem = false;
+  
 
   constructor( private http: HttpClient) { }
 
@@ -35,10 +32,22 @@ export class EventosComponent implements OnInit {
     this.getEventos();  
   }
 
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any)=> evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
+  }
+
+alterarImagem(){
+
+  this.mostrarImagem = !this.mostrarImagem;
+}
+
   getEventos(){
     this.eventos = this.http.get('http://localhost:5000/api/weatherforecast').subscribe(
     Response => {
                   this.eventos = Response;
+                 console.log(Response);
                 }, 
       error => {
                   console.log(error);
