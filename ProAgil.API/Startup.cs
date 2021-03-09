4ba0b94948e80faa712d522644e9e50784d95feb
NicacioLost <nicacio.tecnologia@gmail.com>
2021-03-09 12:35:56 -0300

    Aula 4 Final

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -36,14 +36,16 @@ namespace ProAgil.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            
+           // app.UseHttpsRedirection();
 
             app.UseRouting();
 
